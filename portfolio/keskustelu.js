01592f89bcdb49ca019fcb24bfaf5685d5bc78b5
rrosoii <rn7146@edu.turku.fi>
2025-05-09 13:12:58 +0300

    kommentti console

--- portfolio/keskustelu.js
+++ portfolio/keskustelu.js
@@ -1,1 +1,36 @@
 console.log("Keskustelu sivu ladattu")
+
+function openDialog() {
+    document.getElementById("comment-dialog").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeDialog() {
+    document.getElementById("comment-dialog").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+
+function send(event) {
+    event.preventDefault(); // estää oletuslähetyksen
+
+    const birthdayField = document.getElementById("birthdate");
+    const emailField = document.getElementById("email");
+    const typeField = document.getElementById("type");
+    const usageField = document.getElementById("consent");
+    const bodyField = document.getElementById("message");
+
+    const birthday = birthdayField.value;
+    const email = emailField.value;
+    const type = typeField.value;
+    const usage = usageField.checked; // true tai false
+    const body = bodyField.value;
+
+    console.log("Syntymäpäivä:", birthday);
+    console.log("Sähköposti:", email);
+    console.log("Viestin tyyppi:", type);
+    console.log("Markkinointilupa:", usage);
+    console.log("Viesti:", body);
+
+    closeDialog();
+}
