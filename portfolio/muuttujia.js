c70757d2c13dca8b22001434c87ff6bd4b7280bb
rrosoii <rn7146@edu.turku.fi>
2025-05-22 13:59:57 +0300

    päivämäärä ja aika

--- portfolio/muuttujia.js
+++ portfolio/muuttujia.js
@@ -1,3 +1,4 @@
+console.log("Muuttujat ladattu");
 
 function summa(x, y) {
     alert("Summa: " + (x + y));
@@ -41,3 +42,23 @@ function laskin_jako() {
     jako(eka, toka);
 }
 
+function naytaPaivamaaraJaAika() {
+    const nyt = new Date();
+
+    const paiva = nyt.getDate();
+    const kuukausiNro = nyt.getMonth(); // 0-11
+    const vuosi = nyt.getFullYear();
+
+    const tunnit = nyt.getHours().toString().padStart(2, '0');
+    const minuutit = nyt.getMinutes().toString().padStart(2, '0');
+    const sekunnit = nyt.getSeconds().toString().padStart(2, '0');
+
+    const viikonpaivat = ["sunnuntai", "maanantai", "tiistai", "keskiviikko", "torstai", "perjantai", "lauantai"];
+    const kuukaudet = ["tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu"];
+
+    document.getElementById("paivamaara").innerText = `${paiva}.${kuukausiNro + 1}.${vuosi}`;
+    document.getElementById("aika").innerText = `${tunnit}.${minuutit}.${sekunnit}`;
+    document.getElementById("paivaJaAika").innerText = `${paiva}.${kuukausiNro + 1}.${vuosi} klo ${tunnit}.${minuutit}.${sekunnit}`;
+    document.getElementById("viikonpaiva").innerText = viikonpaivat[nyt.getDay()];
+    document.getElementById("kuukausi").innerText = kuukaudet[kuukausiNro];
+}
