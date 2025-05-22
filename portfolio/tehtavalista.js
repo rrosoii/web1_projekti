0fd5ed733bc7d071887fda3ea3244e0518df1db5
rrosoii <rn7146@edu.turku.fi>
2025-05-22 12:49:29 +0300

    tehtavalista maara

--- portfolio/tehtavalista.js
+++ portfolio/tehtavalista.js
@@ -1,27 +1,33 @@
-console.log("Tehtävälista sivu ladattu")
+console.log("Tehtävälista sivu ladattu");
 
-    let lista = ["Tehtävä 1", "Tehtävä 2"];
+let lista = ["Tehtävä 1", "Tehtävä 2"];
 
-    function paivitaLista() {
-        const ul = document.getElementById("tehtavalista");
-        ul.innerHTML = "";
+function paivitaLista() {
+    const ul = document.getElementById("tehtavalista");
+    ul.innerHTML = ""; 
 
-        for (let tehtava of lista) {
-            const li = document.createElement("li");
-            li.innerHTML = tehtava;
-            ul.appendChild(li);
-        }
+    for (let tehtava of lista) {
+        const li = document.createElement("li");
+        li.innerText = tehtava;
+        ul.appendChild(li);
     }
+}
 
-    function lisaa() {
-        const input = document.getElementById("uusiTehtava");
-        const uusi = input.value.trim();
+function lisaa() {
+    const input = document.getElementById("uusiTehtava");
+    const uusiTehtava = input.value.trim();
+    const maara = document.getElementById("numeroKentta").value;
 
-        if (uusi !== "") {
-            lista.push(uusi);
-            input.value = ""; 
-            paivitaLista();
+    if (uusiTehtava !== "" && maara > 0) {
+        for (let i = 0; i < maara; i++) {
+            lista.push(uusiTehtava);
         }
+        input.value = ""; 
+        document.getElementById("numeroKentta").value = "";
+        paivitaLista(); 
+    } else {
+        alert("Syötä tehtävä ja kelvollinen numero!");
     }
+}
 
-    window.onload = paivitaLista;
+window.onload = paivitaLista;
