5685b79047e0136d50bf79ebe50437bd1be3ec65
rrosoii <rn7146@edu.turku.fi>
2025-05-30 13:43:25 +0300

    js setit

--- portfolio/muuttujia.js
+++ portfolio/muuttujia.js
@@ -62,3 +62,44 @@ function naytaPaivamaaraJaAika() {
     document.getElementById("viikonpaiva").innerText = viikonpaivat[nyt.getDay()];
     document.getElementById("kuukausi").innerText = kuukaudet[kuukausiNro];
 }
+
+    const veijo = new Set(['luku', 'poisto']);
+    const elvira = new Set(['luku', 'kirjoitus', 'muokkaus']);
+    const mehdi = new Set(['luku', 'muokkaus', 'poisto']);
+    const tuuli = new Set(['kirjoitus', 'poisto']);
+
+    const kayttajat = {
+        Veijo: veijo,
+        Elvira: elvira,
+        Mehdi: mehdi,
+        Tuuli: tuuli
+    };
+
+    for (const [nimi, oikeudet] of Object.entries(kayttajat)) {
+        const lista = document.querySelector(`#${nimi} ul`);
+        oikeudet.forEach(oikeus => {
+            const li = document.createElement('li');
+            li.textContent = oikeus;
+            lista.appendChild(li);
+        });
+    }
+
+    function lisaaOikeudet(setti, elementtiId) {
+        const ul = document.getElementById(elementtiId);
+        setti.forEach(oikeus => {
+            const li = document.createElement('li');
+            li.textContent = oikeus;
+            ul.appendChild(li);
+        });
+    }
+
+    const veijotuuliYhd = new Set([...veijo, ...tuuli]);
+    lisaaOikeudet(veijotuuliYhd, 'veijotuuli');
+
+    const mehdielviraYhteiset = new Set([...mehdi].filter(oikeus => elvira.has(oikeus)));
+    lisaaOikeudet(mehdielviraYhteiset, 'mehdielvira');
+
+    const elviratuuliEro = new Set(
+        [...elvira].filter(o => !tuuli.has(o)).concat([...tuuli].filter(o => !elvira.has(o)))
+    );
+    lisaaOikeudet(elviratuuliEro, 'elviratuuli');
